9c4ee61670add7b4304cef0ac7f0bbe38bb3ade7
andrepolischuk <andre.polischuk@gmail.com>
2016-05-13 15:15:31 +0300

    Replace sh to bash

--- mdeb.sh
+++ mdeb.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-
-RESET="\033[0m"
-GREY="\033[0;90m"
-RED="\033[0;91m"
-GREEN="\033[0;92m"
+#!/bin/bash
 
 help='
   Minimal Debian packaging
@@ -19,28 +14,20 @@ help='
     mdeb bar.sh --path /usr/local/share
 '
 
-info() {
-  echo "${GREY}${*}${RESET}"
-}
-
-success() {
-  echo "${GREEN}${*}${RESET}"
-}
-
-error() {
-  echo "${RED}${*}${RESET}"
+fail() {
+  echo "$*"
   exit 1
 }
 
 if ! hash dpkg jq 2>/dev/null ; then
-  info "Enter your password to install dependencies"
+  echo "Enter your password to install dependencies"
   sudo -v
   if [ `uname` == 'Darwin' ]; then
     brew install dpkg jq
   elif [ `uname` == 'Linux' ]; then
     sudo apt-get install dpkg jq
   else
-    error "dpkg and jq is required"
+    fail "dpkg and jq is required"
   fi
 fi
 
@@ -49,8 +36,7 @@ package_version="$(jq -r '.version' package.json)"
 package_description="$(jq -r '.description' package.json)"
 package_maintainer="$(jq -r '.author' package.json)"
 package_path='/usr/share'
-
-declare -a package_files
+package_files=()
 
 while [ -n "$1" ]; do
   param="$1"
@@ -65,19 +51,18 @@ while [ -n "$1" ]; do
       shift
       ;;
     *)
-      package_files[${#package_files[@]}]="$param"
+      package_files=("${package_files[@]}" "$param")
   esac
   shift
 done
 
 if [ -z "$package_files" ]; then
-  error 'You must specify at least one file or directory to add to the Debian package'
+  fail 'You must specify at least one file or directory to add to the Debian package'
 fi
 
 for file in "${package_files[@]}"; do
-  echo "$file"
   if ! [ -e "$file" ]; then
-    error "File does not exist: $file"
+    fail "File does not exist: $file"
   fi
 done
 
@@ -85,12 +70,12 @@ deb_dir="${package_name}_${package_version}_all"
 deb_control_dir="$deb_dir/DEBIAN"
 deb_package_dir="$deb_dir$package_path/$package_name"
 
-info 'Copying files into Debian directory'
+echo 'Copying files into Debian directory'
 
 mkdir -p "$deb_control_dir" "$deb_package_dir"
 cp -r "${package_files[@]}" "$deb_package_dir"
 
-info 'Compiling templates'
+echo 'Compiling templates'
 
 template="Package: $package_name
 Version: $package_version
@@ -102,7 +87,7 @@ Description: $package_description"
 
 echo "$template" > "$deb_control_dir/control"
 
-info 'Calculate md5 sums'
+echo 'Calculate md5 sums'
 
 if hash md5sum 2>/dev/null; then
   find "$deb_dir" -path "$deb_control_dir" -prune -o -type f -print0 | xargs -0 md5sum >> "$deb_control_dir/md5sums"
@@ -113,12 +98,12 @@ elif hash md5 2>/dev/null; then
     done
   }
 else
-  error 'md5 sum program not found'
+  fail 'md5 sum program not found'
 fi
 
-info 'Building Debian package'
+echo 'Building Debian package'
 
 dpkg-deb --build "$deb_dir" > /dev/null
 rm -rf "$deb_dir"
 
-success 'Package was builded successfully'
+echo 'Package was builded successfully'
--- test.sh
+++ test.sh
@@ -1,15 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-RESET="\033[0m"
-RED="\033[0;91m"
-GREEN="\033[0;92m"
-
-success() {
-  echo "${GREEN}${*}${RESET}"
-}
-
-error() {
-  echo "${RED}${*}${RESET}" 1>&2
+fail() {
+  echo "$*" 1>&2
 }
 
 package_name="$(jq -r '.name' package.json)"
@@ -19,34 +11,33 @@ failures=0
 empty_args_test="$(./mdeb.sh)"
 
 if [[ "$empty_args_test" != *'must specify at least one file'* ]]; then
-  error 'Empty arguments test failed'
+  fail 'Empty arguments test failed'
   : $((failures++))
 fi
 
 empty_files_test="$(./mdeb.sh --path /usr/local)"
 
 if [[ "$empty_files_test" != *'must specify at least one file'* ]]; then
-  error 'Empty files test failed'
+  fail 'Empty files test failed'
   : $((failures++))
 fi
 
 wrong_file_test="$(./mdeb.sh package.json foo.js --path /usr/local)"
 
 if [[ "$wrong_file_test" != *'File does not exist: foo.js'* ]]; then
-  error 'Wrong file test failed'
+  fail 'Wrong file test failed'
   : $((failures++))
 fi
 
 success_test="$(./mdeb.sh package.json mdeb.sh --path /usr/local)"
 
 if ! [ -s "${package_name}_${package_version}_all.deb" ]; then
-  error 'Success test failed'
+  fail 'Success test failed'
   : $((failures++))
 fi
 
 if [ "$failures" -eq 0 ]; then
-  success 'All tests passed'
-  exit 0
+  echo 'All tests passed'
 else
   exit 1
 fi
